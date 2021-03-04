3a9eb6551a35f537c6921cf8ccfc6fce4dfc6b07
Rodrigo Nahid <rodrigo@seox.com.br>
2021-03-03 21:43:07 -0300

    Update

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-var header = document.querySelector(".header");
+function scrollSuave(){
+    const accordionList = document.querySelectorAll('.menu a[href^="#"]');
 
-function stickHeader(){
-    if(header.scrollY > 0){
-        header.classList.add("sticky");
-        console.log(header)
-    }else{
-        header.classList.remove("sticky");
+    function activeAccordion(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        var topo = section.offsetTop;
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'end'
+        })
     }
-    
+
+    accordionList.forEach((accordionItem)=>{
+        accordionItem.addEventListener('click', activeAccordion);
+    })
 }
-header.addEventListener('click', stickHeader());
+scrollSuave();
